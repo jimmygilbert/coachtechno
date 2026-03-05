6d75280bfd0d6107a8218e5bf243cb0b852db943
coachtechno <masterjim@gmail.com>
2026-03-04 20:11:35 -0500

    mise a jour des libraries Javascript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
   site: 'https://coachtechno.com',
   integrations: [
-    tailwind(),
     vue(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
